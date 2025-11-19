b3a4343dee63756c24a75e3f196bcdd607c1e869
José Carlos Torres Rivera <jctorresrivera@gmail.com>
2025-11-19 14:31:41 +0000

    genere pantalla de proximamente se lo pondremos a toda la pantalla que s

--- src/app/(private)/settings/page.tsx
+++ src/app/(private)/settings/page.tsx
@@ -4,7 +4,7 @@ import { Settings as SettingsIcon } from "lucide-react";
 export default function SettingsPage() {
     return (
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-            <h1 className="text-2xl font-bold">Ajustes</h1>
+            <h1 className="text-2xl font-bold tracking-tight">Ajustes</h1>
             
             <EmptyState
                 icon={SettingsIcon}
